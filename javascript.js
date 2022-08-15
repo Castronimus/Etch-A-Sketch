cf30cf36e7b88ca3ce6dbc6e743a1df5d054309b
Daniel <onlydcstudy@gmail.com>
2022-08-15 17:54:49 -0400

    Add hover event listener to divs

--- javascript.js
+++ javascript.js
@@ -2,6 +2,14 @@ const grid = document.querySelector(".grid")
 
 for (let counter=1;counter<=256;counter++) {
     const div = document.createElement("div");
-    grid.appendChild(div);
-    
-}
+    grid.appendChild(div);    
+};
+
+const hover = document.querySelectorAll(".grid div");
+
+hover.forEach((div) => {
+    div.addEventListener("mouseover", () => {
+        console.log("UWU YOU HOVER ME")
+    });
+});
+
