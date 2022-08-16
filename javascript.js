91b125ad3e93014f91d5ffd6bf15414ab0ed9c6c
Daniel <onlydcstudy@gmail.com>
2022-08-15 21:51:02 -0400

    Make the button change the size of the grid

--- javascript.js
+++ javascript.js
@@ -1,4 +1,4 @@
-const grid = document.querySelector(".grid")
+let grid = document.querySelector(".grid")
 
 for (let counter=1;counter<=256;counter++) {
     const div = document.createElement("div");
@@ -15,7 +15,16 @@ hover.forEach((div) => {
 });
 
 const button = document.querySelector("button");
+const body = document.querySelector("body");
+const gridDiv = document.querySelector(".grid")
 
 button.addEventListener("click", () => {
     let answer = prompt("Please put the number of squares per side of the new grid");
+    gridDiv.innerHTML = "";
+    grid.style.height = `${answer}em`;
+    grid.style.width = `${answer}em`;
+    for (let counter=1;counter<=answer*answer;counter++) {
+        const div = document.createElement("div");
+        grid.appendChild(div);    
+    }; 
 });
